c70fca72e0e4e3274d75912b1a2936db84c0d138
REZA <alirezaseidi@hotmail.com>
2018-12-13 21:34:14 +0200

    class

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -2,12 +2,30 @@ package org.fasttrackit;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        Car Car = new Car();
+        Car.name = "Dacia";
+        Car.color = "white";
+        Car.doorCount = 4;
+        Car.mileage = 7.5;
+        Car.running = true;
+        Engine engine = new Engine();
+        engine.manufacturer = "Renault";
+        engine.capacity = 1500;
+
+        Car.engine = engine;
+
+        Car.engine.manufacturer = "BMW";
+
+        engine = null;
+
+
+        Car.engine = new Engine();
+
+        Car car2 = new Car();
+        car2.name = "sfa";
+
     }
 }
